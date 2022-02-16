919e3a61020a6539d540a8c15fc2fa5c34224fa6
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-02-16 19:53:31 -0300

    Created component <Button />

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,10 @@
+import { screen } from '@testing-library/react';
+import { Button } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+// describe('<Button />', () => {
+//   it('should render', () => {
+//     renderTheme(<Button />);
+//     expect(screen.getByRole('heading')).toBeInTheDocument();
+//   });
+// });
