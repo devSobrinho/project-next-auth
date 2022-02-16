6574d31ec7f853a0c1d84963ea87a45cb6ae1b7b
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-02-16 19:54:24 -0300

    Created component <TextINput />

---
+++ src/components/TextInput/test.tsx
@@ -0,0 +1,10 @@
+import { screen } from '@testing-library/react';
+import { TextInput } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+// describe('<TextInput />', () => {
+//   it('should render', () => {
+//     renderTheme(<TextInput />);
+//     expect(screen.getByRole('heading')).toBeInTheDocument();
+//   });
+// });
